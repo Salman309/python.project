8b39095653ff813dd86d2d2530c02a96075602b6
salman <salmanfarsi18105@gmail.com>
2024-12-02 23:34:41 +0600

    project uploaded

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = [
+        'title',
+        'price',
+        'discount_price'
+    ]
+
+admin.site.register(Item,ItemAdmin)
+admin.site.register(Order)
+admin.site.register(OrderItem)    
